27f161d8d142b2be864730ca07ccccd4f8510f47
VadimDenisovich <vadimDenis0vich@yandex.com>
2024-10-25 19:33:15 +1000

    Commit

--- main.ts
+++ main.ts
@@ -1,86 +1,71 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, TFolder, moment, PluginSettingTab, Setting, App, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface WeeklyPluginSettings {
+	weeklyFolder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: WeeklyPluginSettings = {
+	weeklyFolder: 'Mind/Diary'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class WeeklyPlugin extends Plugin{
+	settings: WeeklyPluginSettings;
+	private ribbonIconEl: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'create-weekly-note',
+			name: 'Create Weekly Note',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.createWeeklyNote()
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.ribbonIconEl = this.addRibbonIcon('calendar-days', 'Weekly Note',
+			(evt: MouseEvent) => {
+				this.createWeeklyNote();
+			});
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new WeeklyPluginSettingTab(this.app, this))
 	}
 
-	onunload() {
+	async createWeeklyNote() {
+		const startOfWeek = moment().startOf('isoWeek');
+		const endOfWeek = moment().endOf('isoWeek');
+		
+		const folderPath = 'Mind/Diary';
+		const folder = this.app.vault.getAbstractFileByPath(folderPath);
 
-	}
+		if (!folder || !(folder instanceof TFolder)) {
+			await this.app.vault.createFolder(folderPath);
+		}
+
+		const fileName = `(R) ${startOfWeek.format('DD.MM.YY')} - ${endOfWeek.format('DD.MM.YY')}.md`
+		const filePath = `${folderPath}/${fileName}`;
+
+		let file = this.app.vault.getAbstractFileByPath(filePath);
+
+		if (!file || !(file instanceof TFile)) {
+			file = await this.app.vault.create(filePath, '');
+			if (file instanceof TFile) {
+				const leaf = this.app.workspace.getLeaf('tab'); 
+				await leaf.openFile(file);
+			}
+		} else {
+			if (file instanceof TFile) {
+				const leaf = this.app.workspace.getLeaf('tab');
+				await leaf.openFile(file);
+			}
+		}
+	}	
+
+	async onunload() {
+		if (this.ribbonIconEl) {
+			this.ribbonIconEl.remove();
+		}
+	} 
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,44 +76,38 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class WeeklyPluginSettingTab extends PluginSettingTab {
+    plugin: WeeklyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: WeeklyPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        containerEl.createEl('h1', { text: 'Settings for Weekly Plugin' });
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
+        new Setting(containerEl)
+            .setName('Weekly Notes Folder')
+            .setDesc('Folder to store weekly notes')
+            .addText(text => text
+                .setPlaceholder('Enter folder name')
+                .setValue(this.plugin.settings.weeklyFolder)
+                .onChange(async (value) => {
+                    this.plugin.settings.weeklyFolder = value;
+                }));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		.addButton(button => button
+			.setButtonText('Save')
+			.setCta()
+			.onClick(async () => {
+				await this.plugin.saveSettings();
+				new Notice('Settings saved');
+			}));
+}
 }
